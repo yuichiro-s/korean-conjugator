78ffa547c2bc15d930785129db2033def3a3ed4c
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2018-09-09 21:55:59 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='korean_conjugator',
+    version='0.0.1',
+    description='Conjugator of Korean verbs and adjectives.',
+    packages=find_packages(),
+    requires=[])
